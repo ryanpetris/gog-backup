998081913582b3f47438a18eabe098d8f753a8c9
Ryan Petris <ryan@petris.net>
2025-10-30 05:38:21 -0700

    Remove quotes

--- src/gog_backup/common/helpers.py
+++ src/gog_backup/common/helpers.py
@@ -6,6 +6,7 @@ import re
 def sanitize_filename(filename: str) -> str:
     filename = re.sub(r'[<>:"/\\|?!*,.]', ' ', filename)
     filename = re.sub(r'[^\x00-\x7F]+', ' ', filename)
+    filename = re.sub(r'[\'"`]', '', filename)
     filename = re.sub(r'\s+', ' ', filename)
 
     return filename.strip().title()
