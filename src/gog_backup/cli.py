1129c333f8f425f2fff0d57249ffca8969d44e87
Ryan Petris <ryan@petris.net>
2026-01-20 15:12:00 -0700

    Add download summary

--- src/gog_backup/cli.py
+++ src/gog_backup/cli.py
@@ -4,7 +4,7 @@ from rich.console import Console, Group
 from rich.live import Live
 from rich.panel import Panel
 
-from .common import RichDownloadTracker, BlockingThreadPool
+from .common import RichDownloadTracker, BlockingThreadPool, DownloadTracker
 from .gog import get_downloads, GogApi
 from .layout import get_download_progress, get_overall_progress
 
@@ -29,7 +29,31 @@ def gog_backup():
     )
 
     with live:
+        trackers: list[DownloadTracker] = []
+
         for download in get_downloads(overall_progress):
-            pool.submit(GogApi.download_file, download, RichDownloadTracker(download_progress))
+            tracker = RichDownloadTracker(download_progress)
+            trackers.append(tracker)
+
+            pool.submit(GogApi.download_file, download, tracker)
 
         pool.wait()
+
+        complete_files = 0
+        complete_bytes = 0
+        error_files = 0
+        error_bytes = 0
+
+        for tracker in trackers:
+            if tracker.is_complete:
+                complete_files += 1
+                complete_bytes += tracker.total_bytes
+            elif tracker.is_error:
+                error_files += 1
+                error_bytes += tracker.total_bytes
+
+        print("Download Summary:")
+        print(f"Files Downloaded:  {complete_files}")
+        print(f"Bytes Downloaded:  {complete_bytes / 1024 / 1024:.2f} MB")
+        print(f"Files with Errors: {error_files}")
+        print(f"Bytes with Errors: {error_bytes / 1024 / 1024:.2f} MB")
--- src/gog_backup/common/download.py
+++ src/gog_backup/common/download.py
@@ -7,47 +7,81 @@ from rich.progress import Progress, TaskID
 
 
 class DownloadTracker(ABC):
+    @property
+    def is_initialized(self) -> bool:
+        return self._is_initialized
+
+    @property
+    def is_complete(self) -> bool:
+        return self._is_complete
+
+    @property
+    def is_error(self) -> bool:
+        return self._is_error
+
+    @property
+    def total_bytes(self) -> int:
+        return self._total_bytes
+
+    def __init__(self):
+        self._is_initialized: bool = False
+        self._is_complete: bool = False
+        self._is_error: bool = False
+        self._total_bytes: int = 0
+
     def __call__(self, name: str, total: int, **fields: Any):
-        self.init(name, total, **fields)
+        self._init(name, total, **fields)
         return self
 
     def __enter__(self):
+        self._is_initialized = True
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.complete()
+        if exc_type is not None:
+            self._is_error = True
+        else:
+            self._is_complete = True
 
-    @abstractmethod
-    def init(self, name: str, total: int, **fields: Any):
-        raise NotImplementedError
+        self._complete()
 
-    @abstractmethod
     def advance(self, amount: int, **fields: Any):
+        self._total_bytes += amount
+        self._advance(amount, **fields)
+
+    @abstractmethod
+    def _init(self, name: str, total: int, **fields: Any):
         raise NotImplementedError
 
     @abstractmethod
-    def complete(self):
+    def _advance(self, amount: int, **fields: Any):
+        raise NotImplementedError
+
+    @abstractmethod
+    def _complete(self):
         raise NotImplementedError
 
 
 class RichDownloadTracker(DownloadTracker):
     def __init__(self, progress: Progress):
+        super().__init__()
+
         self._progress: Progress = progress
         self._task_id: TaskID | None = None
 
-    def init(self, name: str, total: int, **fields: Any):
+    def _init(self, name: str, total: int, **fields: Any):
         if self._task_id is not None:
             raise Exception("DownloadTracker already initialized")
 
         self._task_id = self._progress.add_task(name, total=total, **fields)
 
-    def advance(self, amount: int, **fields: Any):
+    def _advance(self, amount: int, **fields: Any):
         if self._task_id is None:
             raise Exception("DownloadTracker not initialized")
 
         self._progress.update(self._task_id, advance=amount, **fields)
 
-    def complete(self):
+    def _complete(self):
         if self._task_id is None:
             return
 
@@ -56,11 +90,11 @@ class RichDownloadTracker(DownloadTracker):
 
 
 class DummyDownloadTracker(DownloadTracker):
-    def init(self, name: str, total: int, **fields: Any):
+    def _init(self, name: str, total: int, **fields: Any):
         pass
 
-    def advance(self, amount: int, **fields: Any):
+    def _advance(self, amount: int, **fields: Any):
         pass
 
-    def complete(self):
+    def _complete(self):
         pass
